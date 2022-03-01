e5147e2f6bd44912b5134ffa0ddafe917e5476a4
maeil99 <ismailmohdnorit@gmail.com>
2022-03-01 21:13:09 +0800

    added time and date at accomodation page

--- src/pages/accomodation/Accommodation.tsx
+++ src/pages/accomodation/Accommodation.tsx
@@ -1,52 +1,26 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Card from "../../components/ui/Card/Card";
 import Container from "../../components/ui/Container";
-import { ICovidCasesMalaysia } from "../../shared/interface/covid.interface";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import Button from "../../components/ui/Button/Button";
 
 const Accommodation = () => {
-  const malaysiaCovidURL =
-    "https://disease.sh/v3/covid-19/countries/Malaysia?yesterday=true&strict=true";
-  const [covidCases, setCovidCases] = useState<ICovidCasesMalaysia>();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const date = new Date();
   const navigate = useNavigate();
-  const getCovidCasesPerak = async () => {
-    const res = await axios.get(malaysiaCovidURL);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get("get_started");
 
-    const data = res.data;
-    setCovidCases(data);
+  const navigateToPage = (pageLink: string) => {
+    if (query) {
+      setSearchParams({ get_started: "true", people: "5" });
+    }
+    navigate(pageLink + `?${searchParams}`);
   };
 
-  const navigateToPage = (pagelink: string) => {
-    navigate(pagelink);
-  };
-
-  useEffect(() => {
-    getCovidCasesPerak();
-  }, [date]);
+  const loc = "seri_iskandar";
   return (
     <>
       <Container className="xl:px-0">
-        <div className="space-y-3">
-          <div className="flex flex-col">
-            <p>
-              {date.toDateString()} ,
-              {` ${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()} `}
-            </p>
-            {covidCases && <p>Today's case: {covidCases.todayCases} </p>}
-          </div>
-
-          <Card
-            header="Oyo Hotel"
-            cardType="accommodation"
-            onClick={() => navigateToPage("/")}
-            price={5}
-          >
-            <p>hai</p>
-          </Card>
-        </div>
+        <Button onClick={() => navigateToPage(`/accommodation/${loc}`)}>
+          Search
+        </Button>
       </Container>
     </>
   );
--- src/pages/accomodation/AccommodationList.tsx
+++ src/pages/accomodation/AccommodationList.tsx
@@ -1,17 +1,98 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import CalendarIcon from "../../assets/icons/CalendarIcon";
+import ClockIcon from "../../assets/icons/ClockIcon";
+import Button from "../../components/ui/Button/Button";
+import Card from "../../components/ui/Card/Card";
 import Container from "../../components/ui/Container";
+import { ICovidCasesMalaysia } from "../../shared/interface/covid.interface";
 
 export const AccommodationList = () => {
-    const {place} = useParams();
-    const [searchParams] = useSearchParams();
-    const query = searchParams.get('get_started')
-    const peopleQuery = searchParams.get('people')
-    console.log(place)
-    console.log(query , peopleQuery)
-    
+  //to get tourist destination
+  const { place } = useParams();
+  const [searchParams] = useSearchParams();
+  //to get params
+  const query = searchParams.get("get_started");
+  const peopleQuery = searchParams.get("people");
+  console.log(place);
+  console.log(query, peopleQuery);
+  //call global covid API
+  const malaysiaCovidURL =
+    "https://disease.sh/v3/covid-19/countries/Malaysia?yesterday=true&strict=true";
+  const [covidCases, setCovidCases] = useState<ICovidCasesMalaysia>();
+  const navigate = useNavigate();
+  const getCovidCasesPerak = async () => {
+    const res = await axios.get(malaysiaCovidURL);
+
+    const data = res.data;
+    setCovidCases(data);
+  };
+
+  const navigateToPage = (pageLink: string) => {
+    navigate(pageLink);
+  };
+  const [dateState, setDateState] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => setDateState(new Date()), 30000);
+    getCovidCasesPerak();
+  }, []);
+
   return (
-    <Container>
-      <h1>Accommodation List</h1>
+    <Container className="xl:px-0">
+      <div className="space-y-3">
+        <div className="flex flex-col ">
+          {query && <p> user come from homepage</p>}
+          <div className="flex space-x-3">
+            <div className="flex space-x-2">
+              <CalendarIcon />
+              <p>
+                {dateState.toLocaleDateString("en-GB", {
+                  day: "numeric",
+                  month: "short",
+                  year: "numeric",
+                })}
+              </p>
+            </div>
+            <div className="flex space-x-2">
+              <ClockIcon />
+              <p>
+                {dateState.toLocaleString("en-US", {
+                  hour: "numeric",
+                  minute: "numeric",
+                  hour12: true,
+                })}
+              </p>
+            </div>
+          </div>
+
+          <div className="flex">
+            <p>Location: Malaysia</p>
+          </div>
+          {covidCases && (
+            <div className="flex">
+              <p>Today's case: {covidCases.todayCases.toLocaleString()} </p>
+            </div>
+          )}
+        </div>
+
+        <Card
+          header="Oyo Hotel"
+          cardType="accommodation"
+          onClick={() => navigateToPage("/")}
+          price={5}
+        >
+          <p>hai</p>
+        </Card>
+        {query === "true" ? (
+          <div className=" flex justify-end space-x-4">
+            <Button>Next</Button>
+            <Button>Finish</Button>
+          </div>
+        ) : (
+          <Button onClick={()=> navigateToPage('/')}>Back to Homepage</Button>
+        )}
+      </div>
     </Container>
-  )
+  );
 };
