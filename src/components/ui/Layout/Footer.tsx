5fb818efef95b55d9bf96bbf2d3254c193212fd3
maeil99 <ismailmohdnorit@gmail.com>
2022-03-01 17:36:22 +0800

    upgrade router to version 6

---
+++ src/components/ui/Layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div  className='bg-[#4E95C3] h-[134px] flex justify-center items-center'>
+        <footer className='text-gray-300'>Copyright &copy; 2022 TourWithUs Corp...</footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,56 @@
+import BgImageHomepage from "../../assets/bg-image-homepage.svg";
+// import Button from "../../components/ui/Button/Button";
+import PlanIcon from "../../assets/plan-icon.svg";
+import MoneyIcon from "../../assets/money-icon.svg";
+import { useNavigate } from "react-router-dom";
+// import ThumbsUpIcon from "../../shared/icons/ThumbsUpIcon";
+
+const Home = () => {
+  const navigate = useNavigate()
+  return (
+    <>
+      <div className="h-[720px] relative overflow-hidden w-full">
+        <img
+          src={BgImageHomepage}
+          alt=""
+          className=" object-fill w-full h-[720px]"
+        />
+        <div className="absolute top-0 left-0 px-6 py-4">
+          <p className="pt-[103px] text-[#4E95C3] text-[64px] pl-[430px]">
+            Every journey starts with a single
+          </p>
+          <p className=" text-[#4E95C3] text-[64px] text-left  pl-[530px]">
+            click...
+          </p>
+          <div className="pt-[130px] pb-[187px] pl-[850px] pr-[489px]">
+            {/* <Button
+              bgColor="bg-[#DB0505]"
+              textColor="text-white"
+              borderColor="border-[#DB0505]"
+              textSize="text-[36px]" 
+              onClick={()=> history.push('/accommodation?get_started=true')}
+            >
+              Get Started
+            </Button> */}
+          </div>
+        </div>
+      </div>
+      <div className="h-[465px]  grid grid-cols-3 pt-[149px] text-2xl ">
+        <div className="flex flex-col items-center space-y-[50px]">
+          <p>Plan everything you need here in a single website</p>
+          <img src={PlanIcon} alt="" className="w-[110px] h-[116px]" />
+        </div>
+        <div className="flex flex-col items-center space-y-[50px]">
+          <p>Recommending the best places for you</p>
+          {/* <ThumbsUpIcon className="w-[110px] h-[116px]"/> */}
+        </div>
+        <div className="flex flex-col items-center space-y-[50px]">
+          <p>Make the best choice for your budget</p>
+          <img src={MoneyIcon} alt="" className="w-[110px] h-[116px]" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
