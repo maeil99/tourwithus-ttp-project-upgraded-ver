9f482d1cbfc14ad75d556d2f6ffcadc2a6aba3d3
maeil99 <ismailmohdnorit@gmail.com>
2022-03-08 12:35:09 +0800

    added blank page for attraction

---
+++ src/pages/attraction/Attraction.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Attraction = () => {
+  return (
+    <div>Attraction</div>
+  )
+}
+
+export default Attraction
---
+++ src/pages/attraction/AttractionList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AttractionList = () => {
+  return (
+    <div>AttractionList</div>
+  )
+}
+
+export default AttractionList
