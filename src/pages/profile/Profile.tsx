3145115222432868d3bfaa305325d4213687853b
maeil <83578234+maeil99@users.noreply.github.com>
2022-03-11 04:35:25 +0800

    Develop (#12)
    * finish cab page (#9)
    
    Finish cab page
    
    * Profile page (#11)
    
    * finish cab page (#9) (#10)
    
    Finish cab page
    
    * redeployment with cab page
    
    * added info in profile page

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,16 +1,32 @@
-import { useParams } from "react-router-dom"
-
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const Profile = () => {
-    const {uid} = useParams()
-    console.log(uid)
+  //get user (if logged in)
+  const { user } = useContext<string | any>(AuthContext);
   return (
-    <div className='min-h-screen'>
-
-        <h1>Profile Page</h1>
-        <p>your ID is : {uid}</p>
+    <div className="min-h-screen">
+      <h1 className="text-center font-bold uppercase py-4">Profile page</h1>
+      <div className="flex justify-center">
+        <div className="border p-16 rounded-lg shadow-lg space-y-2">
+          <div className="flex space-x-2">
+            <p>Username: </p>
+            <p className="font-semibold">{user.displayName}</p>
+          </div>
+          <div className="flex space-x-2">
+            <p>Email: </p>
+            <p className="font-semibold">{user.email}</p>
+          </div>
+          <div className="flex space-x-2">
+            <p>Does the email already verified: </p>
+            <p className="font-semibold">
+              {user.emailVerified === false ? "False" : "True"}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
